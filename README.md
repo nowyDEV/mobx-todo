f0bd2acef35790cd4fff2b02f1f9e2ab78db6252
Dominik <nowyTR@users.noreply.github.com>
2019-05-14 03:12:04 +0200

    added readme, rwd styling

---
+++ README.md
@@ -0,0 +1,11 @@
+# Readme
+
+You can find the working demo on [CodeSandbox](https://codesandbox.io/s/y1voq24vv).
+
+# Todos
+
+- unit tests
+- e2e tests
+- switch to mobx-react-lite
+- add pwa support
+- improve ui
--- src/styles/index.js
+++ src/styles/index.js
@@ -7,9 +7,21 @@ export const AppWrapper = styled('main')`
 `
 
 export const AppContainer = styled('div')`
+  margin: auto;
+  position: absolute;
+  left: 50%;
+  transform: translateX(-50%);
   display: block;
   width: 100%;
-  max-width: 1170px;
+  max-width: 420px;
   margin: 0 auto;
-  padding: 0 50px;
+  padding: 0 10px;
+
+  @media only screen and (min-width: 56.25em) {
+    margin-top: 20px;
+  }
+
+  @media only screen and (min-width: 73.125) {
+    margin-top: 100px;
+  }
 `
